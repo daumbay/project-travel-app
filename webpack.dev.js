7cc319a83dece2d046c195a87e2a8153de8dd616
Aditya Matsa <aditya.matsa@icloud.com>
2024-06-01 18:41:20 +0530

    Install babel and its config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,5 +4,14 @@ const webpack = require('webpack');
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: babel-loader
+            }
+        ]
+    }
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,4 +4,13 @@ const webpack = require('webpack');
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: babel-loader
+            }
+        ]
+    }
 }
