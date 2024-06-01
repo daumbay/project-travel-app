cda1a6206766275971c5d04adce365b6bbb4fec0
Aditya Matsa <aditya.matsa@icloud.com>
2024-06-01 15:27:08 +0530

    Serve html page on running server

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,6 @@
+// Configure path
+const path = require('path');
+
 // Configure express app
 const express = require('express');
 const app = express();
@@ -11,10 +14,15 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-// Initialise main project  folder
-app.use(express.static('../client'));
+// Initialise main project folder
+app.use(express.static(path.join(__dirname, '../client')));
 
 // Start server
 const server = app.listen(8000, () => {
     console.log('Server started at localhost:8000');
 })
+
+// Configure GET routes
+app.get('/', (req, res) => {
+    res.sendFile('views/index.html', {root: path.join(__dirname, '../client')});
+});
