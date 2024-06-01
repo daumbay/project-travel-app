b0040d45fa2992088dd6214023ce45a21b11104d
Aditya Matsa <aditya.matsa@icloud.com>
2024-06-01 19:27:18 +0530

    Install sass loaders and their configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,7 +10,11 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: babel-loader
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     }
